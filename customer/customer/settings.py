29c5ccf66ca3652574703e5d25b43a7eef54903e
3RB16 <muhammad3rbi@gmail.com>
2023-05-02 01:20:41 +0300

    fix: docker

--- customer/customer/settings.py
+++ customer/customer/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'customer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
